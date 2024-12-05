888091a56e4f6aa0a8ef9a6c59d5cdb7bca9096e
Ohana,Nisan <nissan.ohana@cognyte.com>
2024-12-05 15:53:19 +0200

    employee finalization work with omer

--- employee.py
+++ employee.py
@@ -3,36 +3,32 @@ class Employee:
         self._e_id = e_id
         self._firstname = firstname
         self._lastname = lastname
-        self._email = self.generate_email(firstname, lastname)
+        self._email = self._generate_email(firstname, lastname)
         self._address = address
-        self._phone_number = self.validate_phone(phone_number)
-        self._gender = self.validate_gender(gender)
-
-    def id_e(self):
-        return self._e_id
+        self._phone_number = self._validate_phone(phone_number)
+        self._gender = self._validate_gender(gender)
 
     @staticmethod
-    def validate_phone(phone_number):
+    def _validate_phone(phone_number):
         if (
             isinstance(phone_number, str)
             and phone_number.startswith("05")
-            and len(phone_number) == 10
-            and phone_number[2:].isdigit()
+            and len(phone_number) == 11
+            and phone_number[3] == "-"
+            and phone_number[0:2].isdigit()
+            and phone_number[4:].isdigit()
         ):
             return phone_number
 
         return None
 
     @staticmethod
-    def validate_gender(gender):
+    def _validate_gender(gender):
         return gender if gender in ["M", "F"] else None
 
     @staticmethod
-    def generate_email(firstname, lastname):
-        return f"{firstname.lower()}.{lastname.lower()}@email.com"
-
-    def get_e_id(self):
-        return self._e_id
+    def _generate_email(firstname, lastname):
+        return f"{firstname}.{lastname}@email.com"
 
     def __repr__(self):
         return (
@@ -41,12 +37,19 @@ class Employee:
         )
 
     def __str__(self):
-        return repr(self)
+        return (
+            f"{self._e_id}, {self._firstname}, {self._lastname}, {self._email}, "
+            f"{self._address}, {self._phone_number}, {self._gender}"
+        )
 
     def __eq__(self, other):
         if isinstance(other, Employee):
             return self._e_id == other.get_e_id()  # Equality based on e_id
+
         return False
 
     def __hash__(self):
         return hash(self._e_id)
+
+    def get_e_id(self):
+        return self._e_id
