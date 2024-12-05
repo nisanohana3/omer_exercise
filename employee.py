1d560bedf285741007e6d89165987e3b44467e73
Ohana,Nisan <nissan.ohana@cognyte.com>
2024-12-05 09:31:54 +0200

    update the employee to protected fields

--- employee.py
+++ employee.py
@@ -1,15 +1,15 @@
 class Employee:
     def __init__(self, e_id, firstname, lastname, address, phone_number, gender):
-        self.__e_id = e_id
-        self.__firstname = firstname
-        self.__lastname = lastname
-        self.__email = self.generate_email(firstname, lastname)
-        self.__address = address
-        self.__phone_number = self.validate_phone(phone_number)
-        self.__gender = self.validate_gender(gender)
+        self._e_id = e_id
+        self._firstname = firstname
+        self._lastname = lastname
+        self._email = self.generate_email(firstname, lastname)
+        self._address = address
+        self._phone_number = self.validate_phone(phone_number)
+        self._gender = self.validate_gender(gender)
 
     def id_e(self):
-        return self.__e_id
+        return self._e_id
 
     @staticmethod
     def validate_phone(phone_number):
@@ -32,12 +32,12 @@ class Employee:
         return f"{firstname.lower()}.{lastname.lower()}@email.com"
 
     def get_e_id(self):
-        return self.__e_id
+        return self._e_id
 
     def __repr__(self):
         return (
-            f"{self.__e_id}, {self.__firstname}, {self.__lastname}, {self.__email}, "
-            f"{self.__address}, {self.__phone_number}, {self.__gender}"
+            f"{self._e_id}, {self._firstname}, {self._lastname}, {self._email}, "
+            f"{self._address}, {self._phone_number}, {self._gender}"
         )
 
     def __str__(self):
@@ -45,8 +45,8 @@ class Employee:
 
     def __eq__(self, other):
         if isinstance(other, Employee):
-            return self.__e_id == other.get_e_id()  # Equality based on e_id
+            return self._e_id == other.get_e_id()  # Equality based on e_id
         return False
 
     def __hash__(self):
-        return hash(self.e_id)
+        return hash(self._e_id)
